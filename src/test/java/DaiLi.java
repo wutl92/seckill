7e79e55fdcefced60353fd7137bbc20509fa4470
wutl92 <wutl92@aliyun.com>
2016-06-16 21:44:59 +0800

    asdas

---
+++ src/test/java/DaiLi.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by Wutl on 2016/6/13.
+ */
+public class DaiLi implements InvocationHandler {
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
